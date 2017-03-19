5c988da6f8309ed9f8f60f91a95718f3b101ebf4
supriti.dan <supriti.dan@rakuten.com>
2017-03-19 20:34:58 +0900

    added README.md and gitignore

---
+++ src/linkedlist/Node.java
@@ -0,0 +1,39 @@
+package linkedlist;
+
+/**
+ * Created by supriti.dan on 3/18/17.
+ */
+public class Node {
+
+	private int data;
+	private Node link;
+
+	public Node(int data, Node link) {
+		this.data = data;
+		this.link = link;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getLink() {
+		return link;
+	}
+
+	public void setLink(Node link) {
+		this.link = link;
+	}
+
+	@Override
+	public String toString() {
+		return "Node{" +
+			   "data=" + data +
+			   ", link=" + link +
+			   '}';
+	}
+}
